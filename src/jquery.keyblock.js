4fe24873cd6a304d9dc81a8f237ffbd9c7161753
Dan Young <danielyoung.private@gmail.com>
2015-02-08 11:17:54 -0600

    update src name references and rebuild minified

--- src/jquery.keyblock.js
+++ src/jquery.keyblock.js
@@ -1,19 +1,19 @@
 (function($) {
-	$.fn.keyfilter = function(whitelist, options) {
+	$.fn.keyblock = function(whitelist, options) {
 		var $this = this;
 	
 		var settings = $.extend( {
 			caseSensitive : false
 		}, options);
 	
-		$this.unbind('keypress.inhibit change.inhibit');
+		$this.unbind('keypress.keyblock change.keyblock');
 	
 		var modifiers = settings.caseSensitive ? 'gm' : 'igm';
 		var blacklist = new RegExp('[^' + whitelist + ']', modifiers);
 	
 		var allowKeys = [0, 8, 13];
 	
-		return $this.bind('keypress.inhibit change.inhibit', function(event) {
+		return $this.bind('keypress.keyblock change.keyblock', function(event) {
 	
 			if(event.type === 'keypress' && String.fromCharCode(event.which).match(blacklist) && $.inArray(event.which, allowKeys) == -1) {
 				event.preventDefault();
