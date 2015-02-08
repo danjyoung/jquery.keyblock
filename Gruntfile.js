2a7e6c680b0df0a8433fbe0555c1060ba2a49870
Dan Young <danielyoung.private@gmail.com>
2015-02-08 11:50:33 -0600

    update build file to copy source and minified to root, add demo html, add build dependency for file copy, add jquery to libs

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,13 +11,24 @@ module.exports = function(grunt) {
         src: 'src/<%= pkg.name %>.js',
         dest: 'build/<%= pkg.name %>.min.js'
       }
-    }
+    },
+	copy: {
+      main: { 
+	    files: [
+		  { src: 'src/jquery.keyblock.js', dest: './jquery.keyblock.js' },
+		  { src: 'build/jquery.keyblock.min.js', dest: './jquery.keyblock.min.js' },
+	    ],
+      },
+	},
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  
+  // Load the plugin that copies the source/build files to root
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['uglify', 'copy']);
 
 };
---
+++ jquery.keyblock.js
@@ -0,0 +1,26 @@
+(function($) {
+	$.fn.keyblock = function(whitelist, options) {
+		var $this = this;
+	
+		var settings = $.extend( {
+			caseSensitive : false
+		}, options);
+	
+		$this.unbind('keypress.keyblock change.keyblock');
+	
+		var modifiers = settings.caseSensitive ? 'gm' : 'igm';
+		var blacklist = new RegExp('[^' + whitelist + ']', modifiers);
+	
+		var allowKeys = [0, 8, 13];
+	
+		return $this.bind('keypress.keyblock change.keyblock', function(event) {
+	
+			if(event.type === 'keypress' && String.fromCharCode(event.which).match(blacklist) && $.inArray(event.which, allowKeys) == -1) {
+				event.preventDefault();
+			}
+			else if (event.type === 'blur' || event.type === 'change') {
+				$this.val($this.val().replace(blacklist, ''));
+			}
+		});
+	};
+})(jQuery);
